8754951dd2bac9377e271cbc03b24931372c71a3
Eric Templeton <erictempleton1@users.noreply.github.com>
2020-11-26 01:03:10 +0000

    adding password field

--- tailwind-login/src/App.js
+++ tailwind-login/src/App.js
@@ -3,16 +3,20 @@ import './tailwind.output.css';
 
 function App() {
   return (
-    <div className="grid grid-cols-3">
-      <div className="shadow-lg rounded-lg p-8">
-        <div className="text-xl py-6">
-          Sign in to your account
+    <div className="shadow-lg rounded-lg p-8 w-1/3">
+      <div className="text-xl py-4">
+        Sign in to your account
         </div>
-        <form>
-          <label className="block" for="email">Email</label>
-          <input className="w-full h-12 focus:outline-none focus:ring-4 focus:ring-blue-100 rounded-md" id="email" type="text" />
-        </form>
-      </div>
+      <form>
+        <div className="py-2">
+          <label className="block py-4" for="email">Email</label>
+          <input className="w-full h-12 focus:outline-none focus:ring-4 focus:ring-blue-200 rounded-md border-gray-200" id="email" type="text" />
+        </div>
+        <div className="py-2">
+          <label className="block py-4" for="password">Password</label>
+          <input className="w-full h-12 focus:outline-none focus:ring-4 focus:ring-blue-200 rounded-md border-gray-200" id="email" type="password" />
+        </div>
+      </form>
     </div>
   );
 }
